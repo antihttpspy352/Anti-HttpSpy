159df817b0103d0b7fc3d5b4a4d53b9001e3d0d4
antihttpspy352 <antihttpspy@gmail.com>
2025-09-28 19:33:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -53,19 +53,30 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const domain = url.origin; // get service full link
+    const method = request.method; // get method 'GET' or 'POST' etc.
     const userAgent = request.headers.get('User-Agent') || ''; // get User-Agent    
     const path = decodeURIComponent(url.pathname).split("/"); // get pathname "pathname[1]" = https://cloudflare.com/path
 
     // Handle Hide URL Requests
     if (path[1] === "api" && path[2] === "hideUrl") {
       const key_url = url.searchParams.get("url"); // get key in '?url=Key'
+      const key_method = url.searchParams.get("method"); // get key in '?method=POST'
       
       // Detect if Key Url is Missing
       if (!key_url) {
         return new Response(`400: Missing url parameter`, { status: 400 });
       }
+
+      // Detect if Key Method is invalid
+      if (key_method && key_method.toUpperCase() !== "GET" && key_method.toUpperCase() !== "POST" && key_method.toUpperCase() !== "DELETE" && key_method.toUpperCase() !== "PUT" && key_method.toUpperCase() !== "PATCH") {
+        return new Response(`400: Invalid method parameter`, { status: 400 });
+      }
       
       const json = { URL: key_url };
+
+      if (key_method) {
+        json.Method = key_method.toUpperCase();
+      }
 
       json.Result = `${domain}/domain/${EncodeText(JSON.stringify(json), ServiceKey)}`;
       return new Response(JSON.stringify(json), {
@@ -76,6 +87,11 @@ export default {
     // Handle Access Encoded Domains
     if (path[1] === "domain") {
       const data = JSON.parse(DecodeText(path[2], ServiceKey));
+
+      // Detect if Method isn't match
+      if (("Method" in data) && data.Method !== method) {
+        return new Response(`405: Method Not Allowed`, { status: 405 });
+      }
 
       let response
       response = await fetch(data.URL, {
